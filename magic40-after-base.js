7db378df0fde55ed209f07e1c79fe6083fa83509
Anna Liao <b89324@gmail.com>
2026-09-20 19:21:26 +0800

    Add clean start for candidate preview students

---
+++ magic40-after-base.js
@@ -0,0 +1,5 @@
+/* magic40 production: a brand-new student must start clean. */
+if (!magic40HadPersonal) {
+  state.tasks={}; state.sessions=[]; state.exams=[]; state.calendarEvents=[]; state.magicMemory=[];
+  state.reviewCompletions={}; state.cancelledReviews={}; state.reviewTimeRecords={};
+}
